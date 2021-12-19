24050d485b193c8ca44bd5b841f473ba2b166a11
HelenBassa <helenbassa@gmail.com>
2021-12-19 12:49:46 +0300

    fix: fix config export-import

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import config from './common/config';
+import { PORT } from './common/config';
 import app from './app';
 
 const start = async () => {
   try {
-    await app.listen(config.PORT);
+    await app.listen(PORT);
   } catch (error) {
     app.log.error(error);
     process.exit(1);
