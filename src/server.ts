a47ab39fcd228e232b23d21823fa88a18183db59
HelenBassa <helenbassa@gmail.com>
2022-01-08 21:18:58 +0300

    feat: add hot-reload

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import * as logger from './common/logger';
 
 const start = async () => {
   try {
-    await app.listen(PORT);
+    await app.listen(PORT, '0.0.0.0', () => {
+      app.log.info(`Server starts on port ${PORT}`);
+      console.log(`Server starts on port ${PORT}`);
+    });
   } catch (error: unknown) {
     logger.fatalLog(app)(error);
   }
