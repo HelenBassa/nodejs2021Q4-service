beac2919d980ef2f85ef72d9a18268ef6597e221
HelenBassa <helenbassa@gmail.com>
2022-01-23 20:44:05 +0300

    feat: add hook for check token on request in app

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import * as logger from './common/logger';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
+import loginRouter from './resources/login/login.router';
+import loginControler from './resources/login/login.controller';
+import { HTTP_CODES } from './constants';
 
 const app = fastify(logger.LOG_CONFIG);
 
@@ -18,30 +21,29 @@ app.addHook('onResponse', (_request, reply, done) => {
   done();
 });
 
+app.addHook('onRequest', (request, reply, done) => {
+  loginControler.checkToken(request, reply);
+  done();
+});
+
 app.addHook('onSend', (_request, reply, payload, done) => {
   Object.assign(reply, { payload });
   done();
 });
 
 app.setErrorHandler((error, request, reply) => {
-  reply.status(500).send(`Internal Server Error - ${error}`);
+  reply
+    .status(HTTP_CODES.INTERNAL_SERVER_ERROR)
+    .send(`Internal Server Error - ${error}`);
   logger.onResponseLog(request, reply);
 });
 
 app.register(userRouter);
 app.register(boardRouter);
 app.register(taskRouter);
+app.register(loginRouter);
 
 process.on('uncaughtException', logger.fatalLog(app));
 process.on('unhandledRejection', logger.fatalLog(app));
-
-/*
-!!! For check restarting docker container ->
-!!! -> uncomment the one of following lines and start the server:
-*/
-
-// throw Error('Oops! Uncaught exception!!!');
-
-// Promise.reject(Error('Oops! Unhandled rejection!!!'));
 
 export default app;
