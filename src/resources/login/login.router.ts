bd48aa3a9afc36a3130ae530ba025056b72d6e2c
HelenBassa <helenbassa@gmail.com>
2022-01-23 20:35:44 +0300

    feat: create login router

---
+++ src/resources/login/login.router.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance } from 'fastify';
+import { LoginBody } from './login.types';
+import loginController from './login.controller';
+
+const loginRouter = async (fastify: FastifyInstance): Promise<void> => {
+  fastify.post<{ Body: LoginBody }>('/login', async (request, reply) => {
+    await loginController.postLogin(request, reply);
+  });
+};
+
+export default loginRouter;
