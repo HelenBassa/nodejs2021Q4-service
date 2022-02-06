cec2f48f2b2d711811e036e8fd6a15efc9d6292b
HelenBassa <helenbassa@gmail.com>
2022-02-06 23:44:53 +0300

    feat: add logger settings

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import { HOST, PORT } from './common/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, HOST);
 }
